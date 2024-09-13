7a0091ff8095fbfe23f43bd60ef83b11e4d481fd
clementus360 <clementus360@gmail.com>
2024-09-13 11:27:10 +0200

    add suspense for search params

--- src/app/dashboard/screening/layout.tsx
+++ src/app/dashboard/screening/layout.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 
 import ImportIcon from "@/assets/import.svg"
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import UploadOverlay from "@/components/UploadOverlay";
 import ScreeningNavigation from "@/components/Dashboard/Screening/ScreeningNavigation";
 
@@ -30,30 +30,32 @@ export default function ScreeningLayout({
     }
 
     return (
-        <div className="flex flex-col gap-8 py-8 px-24">
-            <div className="flex flex-col gap-8">
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-4xl font-bold">Screening</h2>
+        <Suspense>
+            <div className="flex flex-col gap-8 py-8 px-24">
+                <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-2">
+                        <h2 className="text-4xl font-bold">Screening</h2>
+                    </div>
+
+                    <div className="flex items-center justify-center self-center bg-lightViolet border-[0.08rem] border-accent border-dashed w-8/12 py-8 rounded-lg">
+                        <button onClick={openUploadOverlay} className="flex gap-2 items-center bg-primary self-center justify-self-end h-max w-max px-4 py-2 text-white text-sm font-bold rounded-md">
+                            <Image src={ImportIcon} height={14} width={14} alt={"search"} />
+                            <p>Import Screening Applicants CSV</p>
+                        </button>
+                    </div>
                 </div>
 
-                <div className="flex items-center justify-center self-center bg-lightViolet border-[0.08rem] border-accent border-dashed w-8/12 py-8 rounded-lg">
-                    <button onClick={openUploadOverlay} className="flex gap-2 items-center bg-primary self-center justify-self-end h-max w-max px-4 py-2 text-white text-sm font-bold rounded-md">
-                        <Image src={ImportIcon} height={14} width={14} alt={"search"} />
-                        <p>Import Screening Applicants CSV</p>
-                    </button>
-                </div>
+                <section className="flex flex-col gap-8 pb-8 bg-white w-full rounded-md drop-shadow-sm overflow-hidden">
+
+                    <ScreeningNavigation />
+                    {/* Page contnts will appear here */}
+                    {children}
+                </section>
+
+                {isUploadOverlayOpen && (
+                    <UploadOverlay onAddFile={handleAddFile} onClose={closeUploadOverlay} />
+                )}
             </div>
-
-            <section className="flex flex-col gap-8 pb-8 bg-white w-full rounded-md drop-shadow-sm overflow-hidden">
-
-                <ScreeningNavigation />
-                {/* Page contnts will appear here */}
-                {children}
-            </section>
-
-            {isUploadOverlayOpen && (
-                <UploadOverlay onAddFile={handleAddFile} onClose={closeUploadOverlay} />
-            )}
-        </div>
+        </Suspense>
     );
 }
